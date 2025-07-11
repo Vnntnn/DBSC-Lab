88dbf1679923b5123d64bd814b83d3003b23340f
FineNlow <thanabodee.nine@gmail.com>
2025-07-12 00:11:44 +0700

    Add lab1 all assignments.

---
+++ Database System Concept/Lab/Week1/Assignment/6.sql
@@ -0,0 +1,3 @@
+ALTER TABLE title_copy
+MODIFY status char(50);
+DESCRIBE title_copy;
---
+++ Database System Concept/Lab/Week1/Assignment/7.sql
@@ -0,0 +1,7 @@
+CREATE TABLE STAFF (
+    staff_id int(5),
+    first_name VARCHAR(25),
+    last_name VARCHAR(25),
+    address VARCHAR(150)
+);
+DESCRIBE STAFF;
